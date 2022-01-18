ec2b96a5aadcf41f0a124becc162a2fe99e310a1
bo.zhou <riiyzhou@gmail.com>
2022-01-18 22:41:43 +0800

    add date picker

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,17 @@ import SignUp from "./pages/auth/SignUp"
 import Dashboard from "./pages/dashboard/Dashboard"
 import { RequireAuth } from './useAuth'
 import Modal from '@mui/material/Modal';
-import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
 
 const style = {
     position: 'absolute' as 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 600,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
@@ -36,13 +38,17 @@ export default function App() {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Text in a modal
-                    </Typography>
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                    </Typography>
+        <Box component="form" noValidate onSubmit={handleClose} sx={{ mt: 3 }}>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>...</LocalizationProvider>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Submit
+          </Button>
                 </Box>
             </Modal>
             <div className="App">
