33601f810bdcc2db40cff1a69536cf7ad7552aba
Riiy Zhou <riiyzhou@gmail>
2022-02-02 23:28:06 +0800

    add postings query

--- src/graphql.tsx
+++ src/graphql.tsx
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-const QUERY_ACCOUNTS = gql`
+export const QUERY_ACCOUNTS = gql`
   query ACCOUNTS {
     accounts {
       id
@@ -14,13 +14,29 @@ const QUERY_ACCOUNTS = gql`
     }
   }
 `;
-const INSERT_TRANSACTIONS = gql`
+export const INSERT_TRANSACTIONS = gql`
   mutation insertTransaction($comment: String = "", $payee_payer: String = "", $status: bpchar = "", $tags: jsonb = "", $trans_date: date = "", $postings: postings_arr_rel_insert_input = {data: {account_id: "", currency_id: "", quantity: ""}, on_conflict: {constraint: postings_pkey, update_columns: account_id}}) {
     insert_transactions(objects: {comment: $comment, payee_payer: $payee_payer, tags: $tags, status: $status, trans_date: $trans_date, postings: $postings}) {
       affected_rows
       returning {
         id
         trans_date
+      }
+    }
+  }
+`;
+
+export const POSTINGS_QUERY = gql`
+  query Postings {
+    postings {
+      transaction {
+        trans_date
+      }
+      quantity
+      id
+      account {
+        alias_name
+        name
       }
     }
   }
